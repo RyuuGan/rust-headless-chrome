7b401c0e9cc72c72affa0c0dbe35339c72e4c441
Valery Aligorsky <stdammit@gmail.com>
2019-08-18 00:56:55 +0200

    feat(Runtime): add enable and disable runtime, add ExceptionThrownEvent

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -126,6 +126,8 @@ pub enum Event {
     ResponseReceived(network::events::ResponseReceivedEvent),
     #[serde(rename = "Log.entryAdded")]
     LogEntryAdded(logs::events::EntryAddedEvent),
+    #[serde(rename = "Runtime.exceptionThrown")]
+    RuntimeExceptionThrown(runtime::events::ExceptionThrownEvent),
 }
 
 #[derive(Deserialize, Debug, Clone)]
